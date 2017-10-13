f8e5c4e6b9ef1eb65d3d764f2f9069626120a969
kubajestem <kubacpsz@op.pl>
2017-10-13 11:39:37 +0200

    petle_switch

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -8,16 +8,22 @@
 using namespace std;
 
 int main(int argc, char **argv)
+
 {
-    //char imie; //deklaracja zmiennej znakowej
-    char imie [10]; //deklaracja tablicy znakowej
+    int m = 0;
     
-	cout <<"Witaj w C++!" << endl;
-	cout <<"Imię: ";
-    //cin >> imie;
-    cin.getline(imie, 10);
-    cout << "Cześć" << imie << endl;
-    
+    //while (true) {
+        //cout << "Podaj miesiąc: " << endl;
+        //cin >> m;
+        //if ( m <= 12 && m> 0 )
+            //break;
+  //};
+  
+        cout << "Wprowadź imię: " << endl;
+        cin >> m;
+  };
+    cout <<  m << " jeste gupia" << endl;
 	return 0;
 }
+
 
---
+++ petle_znak.cpp
@@ -0,0 +1,24 @@
+/*
+ petle_switch.cpp
+ * Program pobiera numer miesiąca i wyświetla jego nazwę
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+
+{
+   char zn ='*';
+   while (zn == "t" || zn == "T" || zn == n || zn == N) 
+   {
+        cout << "Podaj znak: " << endl;
+        break
+  };
+  
+    
+	return 0;
+}
+
