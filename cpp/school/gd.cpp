956e4046a62ffd93ca8704567ab0ce4e7a70c0df
kubajestem <kubacpsz@op.pl>
2017-10-31 13:30:33 +0100

    repo

---
+++ cpp/school/gd.cpp
@@ -0,0 +1,19 @@
+
+//pobierz dwie liczby całkowite od uzytkownika i wydrukuj większą
+//a,b - licz. całk.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a, b;
+    a = b = 0;
+    cout << "Podaj liczby: ";
+    cin >> a; 
+    if (a > b){
+            cout << "Ania jest gupia";
+	return 0;
+}
---
+++ phyton/witaj.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# osoba = "Adam Słodowina"
+
+osoba = input('Jak się nazywasz?\n')
+
+print("Witaj", osoba, "!") 
+
+rok = 2017
+wiek = int(input('Ile masz lat?\n'))
+print('Urodziłeś się w', rok - wiek, 'roku')
