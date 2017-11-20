e5c73be25c2b82b67f034c8ec434206fb1d356f0
kubajestem <kubacpsz@op.pl>
2017-11-20 20:43:16 +0100

    potega

--- python/potega.py
+++ python/potega.py
@@ -1,36 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# ao = 1
-# a1 = a
-# an = a * ... * a (n-czynników) dla N+ - {01}
+/*
+ * potega.cpp
+ * a0 = 1
+ * a1 = a
+ * an = a*...*a (n-czynników) dla n zaw. N+ - {1}
+ */
 
-def potega_it(podst, wykladnik):
-    """Funkcja oblicza iteracyjne potęgę l. naturalnej"""
-    wynik = 1
-    for i in range(wykladnik):
-        wynik = wynik * podst
-    return wynik
+#include <iostream>
 
+using namespace std;
 
-def main(args):
-    # pobierz od uzytkownika podstawe i wykładnik
-    # i przypisz do odpwoeidznich zmiennyvh
+float potega_it(float x, int n) {
+    float wynik = 1;
+    for(int i=0; i<n; i++) {
+        cout << i << endl;
+    }
+    return wynik;
+}
 
-    a = int(input("Podaj podstawę: "))
-    n = int(input("Podaj wykładnik: "))
-    assert type(a) == int
-    assert type(n) == int
-
-    assert potega_it(100, 0) == 1
-    assert potega_it(100, 1) == 100
-    assert potega_it(2, 3) == 8
-
-    #print("Potęga: ", potega_it(a, n))
-
-    return 0
-
-
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+int main(int argc, char **argv)
+{
+    cout << "Potęga:" << potega_it() << endl;
+	return 0;
+}
