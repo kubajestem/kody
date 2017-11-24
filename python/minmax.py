cb20cfb2a052849b1a805f9aa2957820951e0c30
kubajestem <kubacpsz@op.pl>
2017-11-24 11:40:30 +0100

    minmac

--- python/minmax.py
+++ python/minmax.py
@@ -3,6 +3,15 @@
 from random import randint
 
 
+def minmax(lista):
+    lmin = lista[0]
+    lmax = lista[0]
+    for i, el in enumerate(lista):
+      if el < lmin:
+        lmin = el
+    return lmin
+
+
 def minimum(lista):
     min = lista[0]
     for i in lista:
@@ -25,10 +34,13 @@ def losuj(ile, zakres):
         lista.append(randint(0, zakres))
     return lista
 
+
 def main(args):
     ile = 20
     zakres = 50
     lista = losuj(ile, zakres)
+    assert minimum([7, 4, 9, 1, 3, 0]) == 0
+    assert maximum([7, 4, 9, 1, 3, 0]) == 9
     print(lista)
     print("Minimum: ", minimum(lista))
     print("Maximum: ", maximum(lista))
