d8e4380d90d039ba7da6290bfcb214c58e77c5ad
kubajestem <kubacpsz@op.pl>
2018-01-05 10:58:22 +0100

    pr

---
+++ python/binarne.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# range(0, 7) = [0, 1, 2, 3, 4, 5, 6]
+
+# metoda !DZIEL I ZWYCIĘŻAJ! - można problemy dzielić na mniejsze części, a
+# sposób ich rozwiązania jest taki sam w małych i dużych częściach
+
+
+def wyszukaj_liniowo(l, el):
+    for i in range(0, len(l)):
+        if l[i] == el:
+            return i
+    return -1
+
+
+# int wyszukaj_liniowo(int l[i], int n, int el){
+#    for(int i = 0; i < n: i++){
+#        if (l[i] == el)
+#            return i;
+
+#    }
+#    return -1;
+# }
+
+
+def  wyszukaj_binarnie(lista, el):
+    lewy, prawy = 0, len(lista) - 1
+    while lewy < prawy:
+        srodek = (lewy + prawy) / 2
+
+
+def main(args):
+    lista = [4, 3, 7, 0, 2, 3, 1, 9, -4]
+    lista.sort()
+    el = 3
+    print(wyszukaj_liniowo(lista, el))
+    assert(wyszukaj_liniowo(lista, 8) == -1)
+    wyszukaj_binarnie(lista, el)
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
