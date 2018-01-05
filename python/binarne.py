71681c9bf069e6b54dead9b4b84dcf0fe5320caf
kubajestem <kubacpsz@op.pl>
2018-01-05 11:40:42 +0100

    f

--- python/binarne.py
+++ python/binarne.py
@@ -6,6 +6,8 @@
 # metoda !DZIEL I ZWYCIĘŻAJ! - można problemy dzielić na mniejsze części, a
 # sposób ich rozwiązania jest taki sam w małych i dużych częściach
 
+from math import floor
+
 
 def wyszukaj_liniowo(l, el):
     for i in range(0, len(l)):
@@ -23,20 +25,44 @@ def wyszukaj_liniowo(l, el):
 #    return -1;
 # }
 
-
-def  wyszukaj_binarnie(lista, el):
+def wyszukaj_binarnie(lista, el):
     lewy, prawy = 0, len(lista) - 1
     while lewy < prawy:
-        srodek = (lewy + prawy) / 2
+        srodek = floor((lewy + prawy) / 2)
+        # print(srodek)
+        if el <= lista[srodek]:
+            prawy = srodek
+        else:
+            lewy = srodek + 1
+    if lista[lewy] == el:
+        return lewy
+
+    return -1
+
+
+def wyszukaj_bin_rek(lewy, prawy, lista, el):
+    if lewy > prawy:
+        return -1  # elementu nieznaleziono
+
+    srodek = floor((lewy + prawy) / 2)
+    if el == lista[srodek]:
+        return srodek  # znaleziono element
+
+    if el < lista[srodek]:
+        return wyszukaj_bin_rek(lewy, srodek - 1, lista, el)
+    else:
+        return wyszukaj_bin_rek(srodek + 1, prawy, lista, el)
 
 
 def main(args):
     lista = [4, 3, 7, 0, 2, 3, 1, 9, -4]
     lista.sort()
+    print(lista)
     el = 3
-    print(wyszukaj_liniowo(lista, el))
-    assert(wyszukaj_liniowo(lista, 8) == -1)
-    wyszukaj_binarnie(lista, el)
+    #print(wyszukaj_liniowo(lista, el))
+    #assert(wyszukaj_liniowo(lista, 8) == -1)
+    print(wyszukaj_binarnie(lista, el))
+    print(wyszukaj_bin_rek(0, len(lista) - 1, lista, el))
     return 0
 
 
