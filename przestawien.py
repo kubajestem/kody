b40f4c6a037ab231a66cdb43b0152a9ffc240eed
kubajestem <kubacpsz@op.pl>
2018-02-28 11:53:20 +0100

    szyfr

---
+++ przestawien.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def szyfruj(tekst, klucz):
+    reszta = len(tekst) % klucz
+    szyfrogram = ""
+    
+    if reszta:
+        tekst += (klucz - reszta) * "."
+
+    for i in range(klucz):
+        for j in range(int(len(tekst) / klucz)):
+            # print(i + j * klucz, tekst[i + j * klucz])
+            szyfrogram += tekst[i + j * klucz]
+
+    return szyfrogram
+
+
+
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    for i in range(int(len(szyfrogram) / klucz)):
+        for j in range(klucz):
+            tekst += szyfrogram[i + (j * int(len(szyfrogram) / klucz))]
+            tekst = tekst.replace(".", "")
+
+    return tekst
+
+
+def main(args):
+    tekst = input("Podaj tekst: ")
+
+    klucz = int(input("Podaj klucz: "))
+    while (2 * klucz > len(tekst)):
+        klucz = int(input("Podaj klucz: "))
+
+
+    szyfrogram = szyfruj(tekst, klucz)
+    print("Zaszyfrowany: ", szyfrogram)
+    deszyfrogram = deszyfruj(szyfrogram, klucz)
+    print("Deszyfrowany: ", deszyfrogram)
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
---
+++ vineger.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szyfr_vigenere.py
+#  
+#  
+
+def szyfruj(tekst, haslo):
+    szyfrogram = []
+    litery = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    litera_hasla = 0
+
+    for i in tekst:
+        index = litery.find(i)
+        if index != -1:
+            index += litery.find(haslo[litera_hasla])
+            index %= 26
+
+            szyfrogram.append(litery[index])
+            litera_hasla += 1
+            if litera_hasla == len(haslo):
+                litera_hasla = 0
+        else:
+            szyfrogram.append(i)
+
+    return "".join(szyfrogram)
+    
+
+
+def deszyfruj(szyfrogram, haslo):
+    tekst = []
+    litery = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    litera_hasla = 0
+
+    for i in szyfrogram:
+        index = litery.find(i)
+        if index != -1:
+            index -= litery.find(haslo[litera_hasla])
+            index %= 26
+
+            tekst.append(litery[index])
+            litera_hasla += 1
+            if litera_hasla == len(haslo):
+                litera_hasla = 0
+        else:
+            tekst.append(i)
+
+    return "".join(tekst)
+
+
+
+def main(args):
+    tekst = input("Podaj tekst: ")
+    haslo = input("Podaj hasło: ")
+    tekst = tekst.upper()
+    haslo = haslo.upper()
+
+    szyfrogram = szyfruj(tekst, haslo)
+
+    print("Zaszyfrowane: ", szyfrogram)
+    print("Deszyfrowane: ", deszyfruj(szyfrogram, haslo))
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
