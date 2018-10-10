e1e8140ab14c2d1335b5ffbed360add0381431c6
kubajestem <kubacpsz@op.pl>
2018-10-10 09:39:40 +0200

    BAZY

--- bazaguz.sql
+++ bazaguz.sql
@@ -1,9 +1,31 @@
 -- bazagus.sql
+DROP TABLE IF EXISTS miasta 
+DROP TABLE IF EXISTS dane_demograficzne 
+
 CREATE TABLE miasta (
     id_miasta INTEGER PRIMARY KEY AUTOINCREMENT,
-    nazwa miasta text(30),
+    nazwa miasta TEXT(30),
     wojewodztwo TEXT(30)
 );
+
+CREATE TABLE dane_demograficzne(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    liczba_mieszkancow INTEGER,
+    liczba_kobiet INTEGER,
+    grupa_wiekowa TEXT(15),
+    data_akt DATE,
+    id_miasta INTEGER,
+    FOREIGN KEY (id_miasta) REFERENCES miasta(id_miasta)
+);
+
+CREATE TABLE powierzchnie(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    powierchnia_miasta DECIMAL,
+    powierchnie_zielone DECIMAL,
+    data_akt DATE,
+    id_miasta INTEGER,
+    FOREIGN KEY (id_miasta) REFERENCES miasta(id_miasta)
+    );
 
 -- sqlite3 baza.db <bazagus.sql
 -- sqlite3 baza.db
