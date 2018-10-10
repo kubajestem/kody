0065a8b9a9560c58880f6621a6405f0a7de5e440
kubajestem <kubacpsz@op.pl>
2018-10-10 09:27:08 +0200

    a

---
+++ bazaguz.sql
@@ -0,0 +1,9 @@
+-- bazagus.sql
+CREATE TABLE miasta (
+    id_miasta INTEGER PRIMARY KEY AUTOINCREMENT,
+    nazwa miasta text(30),
+    wojewodztwo TEXT(30)
+);
+
+-- sqlite3 baza.db <bazagus.sql
+-- sqlite3 baza.db
